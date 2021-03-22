0d5e359436d18358851f80e86766a7b1a246eaeb
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-22 16:16:29 +0530

    Added:Part time Hours

--- EmpWage1.java
+++ EmpWage1.java
@@ -2,23 +2,28 @@ public class EmpWage1
 {
 	public static void main(String args[])
 	{
-		int Present=1;
 		int Wage_Per_Hour=20;
-		int Full_Day=8;
+		int Full_Time=10;
+		int Part_Time=8;
 		int Total_Wage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		System.out.println("empCheck= "+empCheck);
 
-			if(empCheck==Present)
+			if(empCheck==0)
 			{
 				System.out.println("Employee is Present");
-				Total_Wage=Wage_Per_Hour*Full_Day;
+				System.out.println("Done Full Time work");
+				Total_Wage = Wage_Per_Hour*Full_Time;
+				System.out.println("Total_Wage="+Total_Wage);
+			}
+			else if(empCheck==1)
+			{
+				System.out.println("Employee is Present");
+				System.out.println("Done Part Time work");
+				Total_Wage = Wage_Per_Hour*Part_Time;
 				System.out.println("Total_Wage="+Total_Wage);
 			}
 			else
-			{
-				System.out.println("Employee is Absent");
-				System.out.println("Total_Wage="+Total_Wage);
-			}
+					System.out.println("Employee is Absent");
 	}
 }
