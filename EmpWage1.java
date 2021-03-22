37d13159be08124a642e64d52c5e670b80f95bf9
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-22 20:55:18 +0530

    UPDATE: iteration until working days 20 or working hrs 100

--- EmpWage1.java
+++ EmpWage1.java
@@ -5,30 +5,38 @@ public class EmpWage1
 		int Wage_Per_Hour=20;
 		int Full_Time=8;
 		int Part_Time=4;
-		int Total_Wage=0;
-		int Working_days=20;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		System.out.println("empCheck= "+empCheck);
-			int empCheck1=(int)empCheck;
-			switch(empCheck1)
-			{
-			case 0:
-				System.out.println("Employee is present");
-				System.out.println("Done Full Time work");
-				Total_Wage = Wage_Per_Hour*Full_Time;
-				break;
-			case 1:
-				System.out.println("Employee is present");
-				System.out.println("Done part Time work");
- 				Total_Wage = Wage_Per_Hour*Part_Time;
-				break;
-			case 2:
-				System.out.println("Employee is Absent");
-				break;
-			default:
-			System.out.println("Invalid");
-			}
-			System.out.println("Total Wage Per Month= "+Total_Wage*20);
+		int Working_Days=0;
+		int Working_Hrs=0;
+		int Total_Working_Hrs=100;
+		int Absent=0;
+		while(Working_Days<=20)
+		{
+				if((Working_Days<=20)&&(Working_Hrs<100))
+				{
+
+					double empCheck=Math.floor(Math.random()*10)%3;
+					int empCheck1=(int)empCheck;
+					switch(empCheck1)
+					{
+					case 0:
+							System.out.println("Employee is absent");
+						break;
+					case 1:
+						Working_Hrs=Working_Hrs+Part_Time;
+						break;
+					case 2:
+						Working_Hrs=Working_Hrs+Full_Time;
+						break;
+					default:
+					System.out.println("Invalid");
+					}
+					Working_Days++;
+				}
+		}
+		Working_Days--;
+		System.out.println("total working hours="+Working_Hrs);
+		System.out.println("total number of days="+Working_Days);
+		System.out.println("Total wages per month="+Working_Hrs*Wage_Per_Hour);
 
 	}
 }
