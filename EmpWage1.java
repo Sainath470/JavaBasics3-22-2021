4db3bd1da5230a7d3960a1fcef83752242d83358
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-22 14:18:50 +0530

    Added: Daily Employee Wage

--- EmpWage1.java
+++ EmpWage1.java
@@ -1,12 +1,24 @@
-public class EmpWage1{
+public class EmpWage1
+{
 	public static void main(String args[])
 	{
 		int Present=1;
+		int Wage_Per_Hour=20;
+		int Full_Day=8;
+		int Total_Wage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		System.out.println("empCheck= "+empCheck);
-		if(empCheck==Present)
-		System.out.println("Employee is Present");
-		else
-		System.out.println("Employee is Absent");
+
+			if(empCheck==Present)
+			{
+				System.out.println("Employee is Present");
+				Total_Wage=Wage_Per_Hour*Full_Day;
+				System.out.println("Total_Wage="+Total_Wage);
+			}
+			else
+			{
+				System.out.println("Employee is Absent");
+				System.out.println("Total_Wage="+Total_Wage);
+			}
 	}
 }
